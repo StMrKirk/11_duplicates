1e13919b60611dcd273e4f46cd2daee4234d3781
StMrKirk <disaseractiv@gmail.com>
2017-09-20 00:46:26 +0300

    Changed programm a bit

--- duplicates.py
+++ duplicates.py
@@ -1,42 +1,40 @@
 import os
 import sys
-import hashlib
-
-
-def hashfile(path, blocksize = 65536):
-    afile = open(path, 'rb')
-    hasher = hashlib.md5()
-    buf = afile.read(blocksize)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(blocksize)
-    afile.close()
-    return hasher.hexdigest()
+from collections import Counter
 
 
 def data_check(directory):
-    same_hash_files = {}
+    same_size_files = {}
     os.chdir(directory)
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            path = os.path.join(dirpath,filename)
-            filename_hash = hashfile(path)
-            if filename_hash in same_hash_files:
-                same_hash_files[filename_hash].append(path)
+            path = os.path.join(dirpath, filename)
+            filename_size = os.stat(path).st_size
+            if filename_size in same_size_files:
+                same_size_files[filename_size].append(filename)
             else:
-                same_hash_files[filename_hash] = [path]
-    return same_hash_files
+                same_size_files[filename_size] = [filename]
+    return same_size_files
+
+
+def check_for_same_name(directory):
+    data_check_result = data_check(directory)
+    for file_size in data_check_result.keys():
+        list_of_lists_file_size = [[file_item, file_item] for file_item in data_check_result[file_size]]
+        same_name__file_dict = [item[1] for item in list_of_lists_file_size if
+                                Counter([item_inner[0] for item_inner in list_of_lists_file_size])[item[0]] > 1]
+        data_check_result[file_size] = same_name__file_dict
+    return data_check_result
 
 
 def look_for_duplicates(directory):
-    duplicates_counter = 0
-    for file_hash in data_check(directory).keys():
-        if len(data_check(directory)[file_hash]) > 1:
-            duplicates_counter += 1
-            print('Дубликаты {}. Пути и имена к файлам:'.format(duplicates_counter))
-            for file in data_check(directory)[file_hash]:
+    data_check_result = check_for_same_name(directory)
+    for file_size in data_check_result.keys():
+        if len(data_check_result[file_size]) > 1:
+            file_set = set(data_check_result[file_size])
+            for file in file_set:
                 print(file)
-        elif len(data_check(directory)[file_hash]) == 1:
+        elif len(data_check_result[file_size]) == 1:
             pass
 
 
